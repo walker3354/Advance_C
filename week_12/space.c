8935e8f2f9a5616e22408cfee594e7a11c8b1fee
walker3354 <walker3354827@gmail.com>
2024-12-07 10:49:25 +0800

    add homeork 12

--- week_12/space.c
+++ week_12/space.c
@@ -1,28 +1,23 @@
 #include "space.h"
 
-tTypeScore score_buf[N]; //preallocated memory space
+tTypeScore score_buf[N]; // preallocated memory space
 
-void get_score_space(tTypeScore **pp_score)
-{
-
-
-
-
-
-
-
-
-
-	  printf("     get_score_space(): giving space numbered %d\n", );
-
-
-
-
+void get_score_space(tTypeScore **pp_score) {
+    for (int i = 0; i < N; i++) {
+        if (score_buf[i].used == 0) {
+            score_buf[i].used = 1;
+            *pp_score = &score_buf[i];
+            score_buf[i].loc = i;
+            printf("     get_score_space(): giving space numbered %d\n", i);
+            break;
+        }
+    }
     return;
 }
 
-void return_score_space (int loc)
-{
-
-    printf("     return_score_space(): return space numbered %d\n", loc);
+void return_score_space(int loc) {
+    if (loc >= 0 && loc < N) {
+        printf("     return_score_space(): return space numbered %d\n", loc);
+        score_buf[loc].used = 0;
+    }
 }
--- week_12/stack.c
+++ week_12/stack.c
@@ -1,69 +1,48 @@
 #include "stack.h"
 
-tStack *create_stack(void)
-{
-   
-
-
-
-
-
-
+tStack *create_stack(void) {
+    tStack *stack_ptr = (tStack *)malloc(sizeof(tStack));
+    stack_ptr->count = 0;
+    stack_ptr->head = NULL;
+    return stack_ptr;
 }
 
-void handle_push_operation(tStack *stack_ptr)
-{
-
-
-	  
-
-
-	      printf ("[Error]  handle_push_operation(): space full \n");
-
-
-	  
-    printf("  handle_push_operation(): enter score value: ");
-    scanf("%d", &score);
-    
-
-    get_score_space();
-
-
-
-
-    
-    return; 
+void handle_push_operation(tStack *stack_ptr) {
+    if (stack_ptr->count >= N)
+        printf("[Error]  handle_push_operation(): space full \n");
+    else {
+        int score;
+        tNode *new_node = (tNode *)malloc(sizeof(tNode));
+        printf("  handle_push_operation(): enter score value: ");
+        scanf("%d", &score);
+        get_score_space(&(new_node->data_ptr));
+        new_node->next = stack_ptr->head;
+        new_node->data_ptr->score = score;
+        stack_ptr->head = new_node;
+        stack_ptr->count++;
+    }
 }
 
-void handle_pop_operation(tStack *stack_ptr)
-{
-
-	  
-
-
-	      printf ("[Error]  handle_pop_operation(): nothing in stack = \n");
-
-
-	  
-
-	  printf("  handle_pop_operation(): poped value: %d\n", );
-	  return_score_space();
-
-
-
-
+void handle_pop_operation(tStack *stack_ptr) {
+    if (stack_ptr->count == 0)
+        printf("[Error]  handle_pop_operation(): nothing in stack = \n");
+    else {
+        tNode *temp_p = stack_ptr->head;
+        printf("  handle_pop_operation(): poped value: %d\n",
+               temp_p->data_ptr->score);
+        return_score_space(temp_p->data_ptr->loc);
+        stack_ptr->head = temp_p->next;
+        stack_ptr->count--;
+        free(temp_p);
+    }
 }
 
-void print_stack_content(tStack *stack_ptr)
-{
-
-
-    
+void print_stack_content(tStack *stack_ptr) {
     printf("   print_stack_content(): stack items -> ");
-
-
-
-
-
+    tNode *temp_p = stack_ptr->head;
+    for (int i = 0; i < stack_ptr->count; i++) {
+        printf(" %d(%d)", temp_p->data_ptr->score, temp_p->data_ptr->loc);
+        temp_p = temp_p->next;
+    }
+    printf("\n");
 }
-
